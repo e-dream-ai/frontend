ef7c8a13c0af8518a915138649f797118ae6dc5e
Alejandro Casillas <ealejandrocasillas@gmail.com>
2023-08-31 15:17:09 -0700

    feat: adding version to main screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world!
-        </p>
+        <p>Hello world!</p>
+        <p>Version 0.1.0</p>
         <a
           className="App-link"
           href="https://reactjs.org"
