317ab692259cee84d99239f4d05f467e3613ae48
Alejandro Casillas <ealejandrocasillas@gmail.com>
2023-10-30 09:54:12 -0700

    feat(dream): delete dream (#18)

---
+++ src/api/dream/mutation/useDeleteDream.ts
@@ -0,0 +1,36 @@
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import { URL } from "constants/api.constants";
+import { ContentType, getRequestHeaders } from "constants/auth.constants";
+import { ApiResponse } from "types/api.types";
+import { Dream } from "types/dream.types";
+
+export const DELETE_DREAM_MUTATION_KEY = "deleteDream";
+
+type MutateFunctionParams = {
+  uuid?: string;
+};
+
+const deleteDream = ({ uuid }: MutateFunctionParams) => {
+  return async () => {
+    return axios
+      .delete(`${URL}/dream/${uuid}`, {
+        headers: getRequestHeaders({
+          contentType: ContentType.none,
+        }),
+      })
+      .then((res) => {
+        console.log({ res });
+        return res.data;
+      });
+  };
+};
+
+export const useDeleteDream = (uuid?: string) => {
+  return useMutation<ApiResponse<{ dream: Dream }>, Error, unknown>(
+    deleteDream({ uuid }),
+    {
+      mutationKey: [DELETE_DREAM_MUTATION_KEY],
+    },
+  );
+};
