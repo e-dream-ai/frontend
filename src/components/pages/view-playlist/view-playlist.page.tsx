d60cfbc00ec95769d7ce1f2b6bda4c7996466017
Alejandro Casillas <ealejandrocasillas@gmail.com>
2023-11-02 09:14:10 -0700

    feat(playlist): creating hooks for api

--- src/components/pages/view-playlist/view-playlist.page.tsx
+++ src/components/pages/view-playlist/view-playlist.page.tsx
@@ -1,14 +1,73 @@
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useUpdatePlaylist } from "api/playlist/mutation/useUpdatePlaylist";
+import { usePlaylist } from "api/playlist/query/usePlaylist";
 import { Button, Input, Row } from "components/shared";
 import Container from "components/shared/container/container";
 import { Column } from "components/shared/row/row";
 import { Section } from "components/shared/section/section";
+import { FORMAT } from "constants/moment.constants";
+import moment from "moment";
+import { useCallback, useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
 import { useTranslation } from "react-i18next";
+import { Navigate, useParams } from "react-router-dom";
+import UpdatePlaylistSchema, {
+  UpdatePlaylistFormValues,
+} from "schemas/update-playlist.schema";
 import { ThumbnailPlaceholder } from "../view-dream/view-dream.styled";
+
+type Params = { id: string };
 
 const SectionID = "playlist";
 
 export const ViewPlaylistPage = () => {
   const { t } = useTranslation();
+  const { id } = useParams<Params>();
+  const playlistId = Number(id) ?? 0;
+  const { data } = usePlaylist(playlistId);
+  const playlist = data?.data?.playlist;
+
+  const [editMode, setEditMode] = useState<boolean>(false);
+  const { isLoading } = useUpdatePlaylist(playlistId);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<UpdatePlaylistFormValues>({
+    resolver: yupResolver(UpdatePlaylistSchema),
+    defaultValues: { name: "" },
+  });
+
+  const handleEdit = (event: React.MouseEvent) => {
+    event.preventDefault();
+    setEditMode(true);
+  };
+
+  const handleCancel = (event: React.MouseEvent) => {
+    event.preventDefault();
+    setEditMode(false);
+  };
+
+  const resetRemotePlaylistForm = useCallback(() => {
+    reset({
+      name: playlist?.name,
+      owner: playlist?.user.email,
+      created_at: moment(playlist?.created_at).format(FORMAT),
+    });
+  }, [reset, playlist]);
+
+  /**
+   * Setting api values to form
+   */
+  useEffect(() => {
+    resetRemotePlaylistForm();
+  }, [reset, resetRemotePlaylistForm]);
+
+  if (!id || !playlistId) {
+    return <Navigate to="/" replace />;
+  }
+
   return (
     <Section id={SectionID}>
       <Container>
@@ -17,37 +76,62 @@ export const ViewPlaylistPage = () => {
 
           <Row justifyContent="space-between">
             <span />
-            <Button
-              type="button"
-              after={<i className="fa fa-pencil" />}
-              // onClick={handleEdit}
-            >
-              {t("page.view_dream.edit")}
-            </Button>
+            <div>
+              {editMode ? (
+                <>
+                  <Button
+                    type="button"
+                    onClick={handleCancel}
+                    // disabled={isLoading}
+                  >
+                    {t("page.view_dream.cancel")}
+                  </Button>
+
+                  <Button
+                    type="submit"
+                    after={<i className="fa fa-save" />}
+                    // isLoading={isLoading}
+                    marginLeft
+                  >
+                    {isLoading
+                      ? t("page.view_dream.saving")
+                      : t("page.view_dream.save")}
+                  </Button>
+                </>
+              ) : (
+                <Button
+                  type="button"
+                  after={<i className="fa fa-pencil" />}
+                  onClick={handleEdit}
+                >
+                  {t("page.view_playlist.edit")}
+                </Button>
+              )}
+            </div>
           </Row>
 
           <Column>
             <Input
-              //   disabled={!editMode}
-              placeholder={t("page.view_dream.name")}
+              disabled={!editMode}
+              placeholder={t("page.view_playlist.name")}
               type="text"
               before={<i className="fa fa-file-video-o" />}
-              //   error={errors.name?.message}
-              //   {...register("name")}
+              error={errors.name?.message}
+              {...register("name")}
             />
             <Input
               disabled
-              placeholder={t("page.view_dream.owner")}
+              placeholder={t("page.view_playlist.owner")}
               type="text"
               before={<i className="fa fa-user" />}
-              //   {...register("owner")}
+              {...register("owner")}
             />
             <Input
               disabled
-              placeholder={t("page.view_dream.created")}
+              placeholder={t("page.view_playlist.created")}
               type="text"
               before={<i className="fa fa-calendar" />}
-              //   {...register("created_at")}
+              {...register("created_at")}
             />
           </Column>
           <Row
@@ -55,7 +139,7 @@ export const ViewPlaylistPage = () => {
             alignItems="center"
             style={{ marginTop: "5rem" }}
           >
-            <h3>{t("page.view_dream.thumbnail")}</h3>
+            <h3>{t("page.view_playlist.thumbnail")}</h3>
           </Row>
           <Row>
             <ThumbnailPlaceholder>
