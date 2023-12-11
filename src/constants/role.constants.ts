5a438c7c982943f3de603fa61d35ea7fa29349dd
Alejandro Casillas <ealejandrocasillas@gmail.com>
2023-12-11 15:27:57 -0700

    feat(user): adding user role description

--- src/constants/role.constants.ts
+++ src/constants/role.constants.ts
@@ -4,3 +4,8 @@ export const ROLES: { USER_GROUP: RoleType; ADMIN_GROUP: RoleType } = {
   USER_GROUP: "user-group",
   ADMIN_GROUP: "admin-group",
 };
+
+export const ROLES_NAMES = {
+  [ROLES.USER_GROUP]: "roles.user_group",
+  [ROLES.ADMIN_GROUP]: "roles.admin_group",
+};
