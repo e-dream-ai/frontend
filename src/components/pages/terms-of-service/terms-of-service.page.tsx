4f1929956f902a37296b5630a4bc782553e27586
Scott Draves <spot@draves.org>
2023-12-20 10:59:46 -0500

    feat: adding terms of service, html trial

--- src/components/pages/terms-of-service/terms-of-service.page.tsx
+++ src/components/pages/terms-of-service/terms-of-service.page.tsx
@@ -1,6 +1,5 @@
 import { Row } from "components/shared";
 import Container from "components/shared/container/container";
-import { LoremIpsum8P } from "components/shared/lorem-ipsum/lorem-ipsum";
 import { Section } from "components/shared/section/section";
 import Text from "components/shared/text/text";
 import { useTranslation } from "react-i18next";
@@ -15,7 +14,25 @@ export const TermsOfServicePage: React.FC = () => {
       <Section id={SECTION_ID}>
         <Row justifyContent="space-between" separator />
         <Text>
-          <LoremIpsum8P />
+          <ol>
+            <li>
+              <b>Acceptance of Terms of Service</b>
+              <ol>
+                <li>
+                  These Terms of Service, which include our End User License
+                  Agreement (&ldquo;EULA&rdquo;) govern your use of the e-dream
+                  service, hereafter, &ldquo;The Service&rdquo;.
+                </li>
+                <li>more details</li>
+              </ol>
+            </li>
+            <li>
+              <b>Changes to Terms of Service.</b> The company may, from time to
+              time, change these Terms of Service. We will post announcements
+              through social media and on
+              <a href="https://e-dream.ai/">e-dream.ai</a> when we do so.
+            </li>
+          </ol>
         </Text>
       </Section>
     </Container>
