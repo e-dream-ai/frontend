c7bc57ea6c5dd9902cb784204738e542753bd396
Scott Draves <spot@draves.org>
2023-12-31 14:55:08 -0500

    fix: add placeholder download link

--- src/components/pages/install/install.page.tsx
+++ src/components/pages/install/install.page.tsx
@@ -1,4 +1,5 @@
 import { Row } from "@/components/shared";
+import { Anchor } from "@/components/shared";
 import Container from "@/components/shared/container/container";
 import { Section } from "@/components/shared/section/section";
 import Text from "@/components/shared/text/text";
@@ -14,13 +15,16 @@ export const InstallPage: React.FC = () => {
       <Section id={SECTION_ID}>
         <Row justifyContent="space-between" separator />
         <Text>
-          <p>
-	    Download and double click a DMG with a native Mac
-	    application. Run installer or drag to applications.
-	  </p>
 	  <p>
 	    Requirements: Mac OS version X or better, x86 or Apple
 	    Silicon.
+	  </p>
+          <p>
+	    <Anchor href="https://something.com/somewhere.dmg">
+	      Download
+	    </Anchor>
+	    and double click a DMG with a native Mac application. Run
+	    installer or drag to applications.
 	  </p>
 	  <p>
 	    Open the settings to log in. Press F1 key for help and F2
