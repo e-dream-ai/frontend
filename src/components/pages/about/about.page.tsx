3738a5f49e12de31d927084d7d28ca154882872e
Scott Draves <spot@draves.org>
2024-02-09 18:08:06 -0500

    fix: improve about text

--- src/components/pages/about/about.page.tsx
+++ src/components/pages/about/about.page.tsx
@@ -15,12 +15,8 @@ export const AboutPage: React.FC = () => {
         <Row justifyContent="space-between" separator />
         <Text>
 	  <p>
-	    E-dream is a community for generative AI visuals and all
-	    animated artwork.
-	  </p>
-	  <p>
-	    Discover and experience the latest animations from the
-	    greatest artists.
+	    Discover and experience the latest generative AI animations
+	    from the greatest artists.
 	  </p>
 	  <p>
 	    Install e-dream on your Mac to get the highest quality
@@ -30,15 +26,11 @@ export const AboutPage: React.FC = () => {
 	    adjustable super slow and smooth playback.
 	  </p>
 	  <p>
-	    Curate playlists or upload your own creations and earn
-	    based on views of your work.
-	  </p>
-	  <p>
-	    Connect and learn from other fans on Discord or social
+	    Curate playlists, connect and learn from other fans on Discord or social
 	    media. Become a creator yourself!
 	  </p>
 	  <p>
-	    Create an account, install the native client, and begin
+	    Signup, install the native client, and begin
 	    the experience.
 	  </p>
         </Text>
