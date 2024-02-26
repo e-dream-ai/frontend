ec7baa750a1f21e3e5cc67c6a5ca67a9dcfe79d1
Max Carlson <>
2024-02-26 10:53:09 +0200

    Basic bugsnag integration

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,32 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./i18n";
+import React from 'react'
+import Bugsnag from '@bugsnag/js'
+import BugsnagPluginReact from '@bugsnag/plugin-react'
+import BugsnagPerformance from '@bugsnag/browser-performance'
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
-root.render(<App />);
+
+if (import.meta.env.PROD) {
+    Bugsnag.start({
+      apiKey: '563a8a8c8ca5c81400b1a137177abcab',
+      plugins: [new BugsnagPluginReact()]
+    })
+    BugsnagPerformance.start({ apiKey: '563a8a8c8ca5c81400b1a137177abcab' })
+
+    const ErrorBoundary = Bugsnag.getPlugin('react')
+      .createErrorBoundary(React)
+
+    root.render(
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
+    );
+} else {
+  root.render(
+    <App />
+  );
+}
