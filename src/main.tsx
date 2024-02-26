00b74c7c259dd9181dd824b757b4f6ee09785c2d
dealparadize <ealejandrocasillas@gmail.com>
2024-02-26 15:34:21 -0700

    fix(bugsnag): bugsnag type error

--- src/main.tsx
+++ src/main.tsx
@@ -1,32 +1,29 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./i18n";
-import React from 'react'
-import Bugsnag from '@bugsnag/js'
-import BugsnagPluginReact from '@bugsnag/plugin-react'
-import BugsnagPerformance from '@bugsnag/browser-performance'
+import React from "react";
+import Bugsnag from "@bugsnag/js";
+import BugsnagPluginReact from "@bugsnag/plugin-react";
+import BugsnagPerformance from "@bugsnag/browser-performance";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
 
 if (import.meta.env.PROD) {
-    Bugsnag.start({
-      apiKey: '563a8a8c8ca5c81400b1a137177abcab',
-      plugins: [new BugsnagPluginReact()]
-    })
-    BugsnagPerformance.start({ apiKey: '563a8a8c8ca5c81400b1a137177abcab' })
+  Bugsnag.start({
+    apiKey: "563a8a8c8ca5c81400b1a137177abcab",
+    plugins: [new BugsnagPluginReact()],
+  });
+  BugsnagPerformance.start({ apiKey: "563a8a8c8ca5c81400b1a137177abcab" });
 
-    const ErrorBoundary = Bugsnag.getPlugin('react')
-      .createErrorBoundary(React)
+  const ErrorBoundary = Bugsnag.getPlugin("react")!.createErrorBoundary(React);
 
-    root.render(
-      <ErrorBoundary>
-        <App />
-      </ErrorBoundary>
-    );
-} else {
   root.render(
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>,
   );
+} else {
+  root.render(<App />);
 }
