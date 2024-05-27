74b135173389831ec0d47c550b6e48a7faf85750
dealparadize <ealejandrocasillas@gmail.com>
2024-05-27 16:31:50 -0700

    style(checkbox): change style for checkbox

--- src/components/shared/checkbox/checkbox.styled.tsx
+++ src/components/shared/checkbox/checkbox.styled.tsx
@@ -1,5 +1,69 @@
 import styled from "styled-components";
 
+export const CheckboxContainer = styled.label`
+  display: inline-block;
+  position: relative;
+  padding-left: 34px;
+  margin-bottom: 24px;
+  cursor: pointer;
+  font-size: 22px;
+  user-select: none;
+
+  input {
+    position: absolute;
+    opacity: 0;
+    cursor: pointer;
+    height: 0;
+    width: 0;
+  }
+
+  /**
+   * custom checkbox 
+   */
+  span {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 25px;
+    width: 25px;
+    background-color: ${(props) => props.theme.inputBackgroundEnabledColor};
+    border-radius: 5px;
+  }
+
+  /**
+   * add a background color when the checkbox is checked
+   */
+  input:checked ~ span {
+    background-color: ${(props) => props.theme.colorPrimary};
+  }
+
+  /**
+   * create the checkmark/indicator, hidden when not checked
+   */
+  span:after {
+    content: "";
+    position: absolute;
+    display: none;
+  }
+
+  /**
+   * show the checkmark when checked 
+   */
+  input:checked ~ span:after {
+    display: block;
+  }
+
+  span:after {
+    left: 9px;
+    top: 5px;
+    width: 5px;
+    height: 10px;
+    border: solid white;
+    border-width: 0 3px 3px 0;
+    transform: rotate(45deg);
+  }
+`;
+
 export const StyledCheckbox = styled.input`
   cursor: pointer;
   margin: 0.5rem;
--- src/components/shared/checkbox/checkbox.tsx
+++ src/components/shared/checkbox/checkbox.tsx
@@ -1,19 +1,25 @@
 import React from "react";
 import { InputError, InputGroup, InputRow } from "../input/input.styled";
-import { CheckboxLabel, StyledCheckbox } from "./checkbox.styled";
+import {
+  CheckboxContainer,
+  CheckboxLabel,
+  StyledCheckbox,
+} from "./checkbox.styled";
 
 type CheckboxProps = React.InputHTMLAttributes<HTMLInputElement> & {
   error?: string;
 };
 
-// eslint-disable-next-line react/display-name
 export const Checkbox: React.FC<CheckboxProps> = React.forwardRef<
   HTMLInputElement,
   CheckboxProps
 >(({ children, error, ...props }, ref) => (
   <InputGroup>
     <InputRow>
-      <StyledCheckbox ref={ref} type="checkbox" {...props} />
+      <CheckboxContainer>
+        <StyledCheckbox ref={ref} type="checkbox" {...props} />
+        <span />
+      </CheckboxContainer>
       <CheckboxLabel>{children}</CheckboxLabel>
     </InputRow>
     {error && <InputError>{error}</InputError>}
