b8476b7421a59bb1f0f41d3d43c33a4b3b724720
dealparadize <ealejandrocasillas@gmail.com>
2024-09-12 12:58:06 -0700

    fix: update logout call

--- src/api/auth/useLogout.ts
+++ src/api/auth/useLogout.ts
@@ -1,22 +1,15 @@
 import { useMutation } from "@tanstack/react-query";
-import { ContentType, getRequestHeaders } from "@/constants/auth.constants";
+// import { ContentType, getRequestHeaders } from "@/constants/auth.constants";
 import { LogoutRequestValues } from "@/schemas/logout.schema";
 import { ApiResponse } from "@/types/api.types";
 import { axiosClient } from "@/client/axios.client";
 
 export const LOGOUT_MUTATION_KEY = "logout";
 
-const logout = async (values: LogoutRequestValues) => {
-  return axiosClient
-    .post(`/v2/auth/logout`, values, {
-      headers: getRequestHeaders({
-        contentType: ContentType.json,
-      }),
-      validateStatus: (status) => status >= 200 && status < 303, // Consider 302 as success on logout redirection
-    })
-    .then((res) => {
-      return res.data;
-    });
+const logout = async () => {
+  return axiosClient.get(`/v2/auth/logout`).then((res) => {
+    return res.data;
+  });
 };
 
 export const useLogout = () => {
