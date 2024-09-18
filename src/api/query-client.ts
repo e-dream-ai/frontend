be871abf1fe2f2bd7a4ff4cb59f0ab3b5bb4fb16
dealparadize <ealejandrocasillas@gmail.com>
2024-09-18 14:14:01 -0700

    feat: update query client default options

--- src/api/query-client.ts
+++ src/api/query-client.ts
@@ -1,5 +1,37 @@
 import { QueryClient } from "@tanstack/react-query";
+import axios from "axios";
 
-export const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: (failureCount, error) => {
+        // Check if the error is an AxiosError
+        if (axios.isAxiosError(error)) {
+          console.log({ error });
+          if (error.response) {
+            // Don't retry on 401 errors
+            if (error.response.status === 401) {
+              console.log("401 not retry");
+              return false;
+            }
+            // Retry on 5xx errors
+            if (error.response.status >= 500) {
+              return failureCount < 3;
+            }
+          }
+          // Retry on network errors
+          if (!error.response) {
+            return failureCount < 3;
+          }
+        }
+        // For non-Axios errors, retry up to 3 times
+        return failureCount < 3;
+      },
+      onError: () => {
+        //
+      },
+    },
+  },
+});
 
 export default queryClient;
