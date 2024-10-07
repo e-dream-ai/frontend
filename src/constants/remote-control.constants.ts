068cef2dd060ec68ecc69e5d2c4a0a6aaf77604f
dealparadize <ealejandrocasillas@gmail.com>
2024-10-07 16:30:56 -0700

    fix: fixing description on upvote remote event

--- src/constants/remote-control.constants.ts
+++ src/constants/remote-control.constants.ts
@@ -8,7 +8,7 @@ export const REMOTE_CONTROLS: { [key: string]: RemoteControlAction } = {
   PLAYING: { event: "playing", key: "" },
   PLAY_DREAM: { event: "play_dream", key: "" },
   PLAY_PLAYLIST: { event: "play_playlist", key: "" },
-  LIKE_DREAM: { event: "like", key: "" },
+  LIKE_DREAM: { event: "like", key: "↑" },
   DISLIKE_DREAM: { event: "dislike", key: "" },
   LIKE_CURRENT_DREAM: { event: "like_current_dream", key: "↑" },
   DISLIKE_CURRENT_DREAM: { event: "dislike_current_dream", key: "↓" },
@@ -45,6 +45,8 @@ export const REMOTE_CONTROLS_TRANSLATIONS = {
   [REMOTE_CONTROLS.PLAY_DREAM.event]: "components.remote_control.play_dream",
   [REMOTE_CONTROLS.PLAY_PLAYLIST.event]:
     "components.remote_control.play_playlist",
+  [REMOTE_CONTROLS.LIKE_DREAM.event]: "components.remote_control.like",
+  [REMOTE_CONTROLS.DISLIKE_DREAM.event]: "components.remote_control.dislike",
   [REMOTE_CONTROLS.LIKE_CURRENT_DREAM.event]: "components.remote_control.like",
   [REMOTE_CONTROLS.DISLIKE_CURRENT_DREAM.event]:
     "components.remote_control.dislike",
