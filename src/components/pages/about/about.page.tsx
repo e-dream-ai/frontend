3043833b38263d4e9c39e4b8f23bf78f0754f38e
Scott Draves <spot@draves.org>
2024-11-18 11:02:38 -0500

    (fix) add structure and improve language

--- src/components/pages/about/about.page.tsx
+++ src/components/pages/about/about.page.tsx
@@ -20,19 +20,21 @@ export const AboutPage: React.FC = () => {
             Discover and experience the latest generative AI visuals from the
             greatest artists.
           </p>
-          <p>
-            Install e-dream on your Mac to get the best experience. The e-dream
-	    app provides unsurpassed support for ambient interaction,
-	    including adjustable slow and smooth playback.
-          </p>
-          <p>
-            Install the screensaver so it runs automatically when you're not
-	    at your computer.
-          </p>
-          <p>
-            Install the web app on your phone to remote-control your e-dream
-            from anywhere.
-          </p>
+	  <ul>
+            <li>
+	      Install e-dream on your Mac to get the best experience. The e-dream
+	      app provides unsurpassed support for ambient interaction,
+	      including adjustable slow and smooth playback.
+	    </li>
+	    <li>
+	      Install the screensaver so it runs automatically when you're not
+	      using your computer.
+	    </li>
+	    <li>
+	      Install the web app on your phone to remote-control your e-dream
+	      from anywhere.
+	    </li>
+	  </ul>
           <p>
             Curate playlists, connect and learn from other fans on Discord or
             social media. Become a creator yourself!
