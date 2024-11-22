4569a9ef7e4457fdd1770e26feda3b64d01fb413
dealparadize <ealejandrocasillas@gmail.com>
2024-11-22 16:04:14 -0700

    feat: about page blue button #174

--- src/components/pages/about/about.page.tsx
+++ src/components/pages/about/about.page.tsx
@@ -1,50 +1,103 @@
-import { Row } from "@/components/shared";
+import { Button, Column, Row } from "@/components/shared";
 import { Anchor } from "@/components/shared";
+import { Card } from "@/components/shared/card/card";
 import Container from "@/components/shared/container/container";
 import { Section } from "@/components/shared/section/section";
 import Text from "@/components/shared/text/text";
 import { ROUTES } from "@/constants/routes.constants";
+import router from "@/routes/router";
 import { useTranslation } from "react-i18next";
 
 const SECTION_ID = "about";
 
 export const AboutPage: React.FC = () => {
   const { t } = useTranslation();
+
+  const handleCreateAccount = () => {
+    router.navigate(ROUTES.SIGNIN);
+  };
+
+  const handleInstallApp = () => {
+    router.navigate(ROUTES.INSTALL);
+  };
+
   return (
     <Container>
       <h2>{t("page.about.title")}</h2>
       <Section id={SECTION_ID}>
         <Row justifyContent="space-between" separator />
+
+        <Row flexWrap={["wrap", "nowrap", "nowrap"]}>
+          <Column
+            flex={[
+              "0 0 100%", // full width on small screens
+              "0 0 50%", // half width on medium screens and up
+            ]}
+            pr={[0, 1, 2]}
+          >
+            <Card
+              style={{ display: "flex" }}
+              flex="auto"
+              mt={3}
+              px={[2, 3, 4]}
+              py={4}
+              justifyContent="center"
+            >
+              <Button buttonType="secondary" onClick={handleCreateAccount}>
+                Create Account
+              </Button>
+            </Card>
+          </Column>
+          <Column
+            flex={[
+              "0 0 100%", // full width on small screens
+              "0 0 50%", // half width on medium screens and up
+            ]}
+            pr={[0, 1, 2]}
+          >
+            <Card
+              style={{ display: "flex" }}
+              flex="auto"
+              mt={3}
+              px={[2, 3, 4]}
+              py={4}
+              justifyContent="center"
+            >
+              <Button buttonType="secondary" onClick={handleInstallApp}>
+                Install App
+              </Button>
+            </Card>
+          </Column>
+        </Row>
+
         <Text>
           <p>
             Discover and experience the latest generative AI visuals from the
             greatest artists.
           </p>
-	  <ul>
+          <ul>
             <li>
-	      Install e-dream on your Mac to get the best experience. The e-dream
-	      app provides unsurpassed support for ambient interaction,
-	      including adjustable slow and smooth playback.
-	    </li>
-	    <li>
-	      Install the screensaver so it runs automatically when you're not
-	      using your computer.
-	    </li>
-	    <li>
-	      Install the web app on your phone to remote-control your e-dream
-	      from anywhere.
-	    </li>
-	  </ul>
+              Install e-dream on your Mac to get the best experience. The
+              e-dream app provides unsurpassed support for ambient interaction,
+              including adjustable slow and smooth playback.
+            </li>
+            <li>
+              Install the screensaver so it runs automatically when you're not
+              using your computer.
+            </li>
+            <li>
+              Install the web app on your phone to remote-control your e-dream
+              from anywhere.
+            </li>
+          </ul>
           <p>
             Curate playlists, connect and learn from other fans on Discord or
             social media. Become a creator yourself!
           </p>
           <p>
-            <Anchor href={ROUTES.SIGNUP}>
-              Create an account
-            </Anchor>
-            , <Anchor href="/install">install the app</Anchor>,
-            and begin the experience.
+            <Anchor href={ROUTES.SIGNUP}>Create an account</Anchor>,{" "}
+            <Anchor href="/install">install the app</Anchor>, and begin the
+            experience.
           </p>
         </Text>
       </Section>
