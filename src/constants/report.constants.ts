42c3dc4683e599a0389e8d8cf4a95431b688a581
Scott Draves <spot@draves.org>
2025-03-04 20:25:25 -0500

    fix: clean up wording of report form

--- src/constants/report.constants.ts
+++ src/constants/report.constants.ts
@@ -3,13 +3,13 @@ export const UNLICENSED_TYPE_ID = 4;
 
 export const TYPES = [
   { id: 1, short: "Spam", type: "Spam content" },
-  { id: 2, short: "Nsfw", type: "Not safe for work (NSFW)" },
-  { id: 3, short: "Spam", type: "Contains visible title, watermark, or bug" },
+  { id: 2, short: "NSFW", type: "Not safe for work (NSFW)" },
+  { id: 3, short: "Bug", type: "Contains visible title, watermark, or bug" },
   {
     id: 4,
     short: "Unlicensed",
-    type: "Potentially unlicensed (original source needed)",
+    type: "Unlicensed",
   },
-  { id: 5, short: "Illegal", type: "Illegal or harassing material" },
-  { id: 6, short: "Other", type: "Other content issue" },
+  { id: 5, short: "Illegal", type: "Illegal or harassing" },
+  { id: 6, short: "Other", type: "Other" },
 ];
