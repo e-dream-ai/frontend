9482e4715a2b5f9c3d15ab1a3c9b992d3e83c887
Scott Draves <spot@draves.org>
2025-06-14 12:38:31 -0400

    add users manual, fix formatting, rename link in terms of service

--- src/components/pages/help/help.page.tsx
+++ src/components/pages/help/help.page.tsx
@@ -14,13 +14,13 @@ export const HelpPage: React.FC = () => {
       <h2>{t("page.help.title")}</h2>
       <Section id={SECTION_ID}>
         <Row justifyContent="space-between" separator />
-	<h3>Links to Documentation</h3>
 	<Text>
-	  See the Users Manual for explanation of the app and website,
-	  {" "}<Anchor href="https://docs.google.com/document/u/1/d/e/2PACX-1vTQnJMCLOqenrCADZyrXxgBTahQ4sPyRRj7GrhMEu_DkmScRRGOjRJQmd2rkH1-_K0WRjfGYd04rhJB/pub">Creators' Guide</Anchor>, and the
-	  {" "}<Anchor href="https://github.com/e-dream-ai/python-api">Python API</Anchor>.
+          See the {" "}<Anchor href="https://docs.google.com/document/d/e/2PACX-1vRK77s1OWASmJ_r8ZZXkFzNxpSxoqcOVn2-0shwXZquzxsWfzO86oeH_9Q09IVmb5gaUnuHZQP0ZihG/pub">Users' Manual</Anchor> for explanation of the app and website, the
+	  {" "}<Anchor href="https://docs.google.com/document/u/1/d/e/2PACX-1vTQnJMCLOqenrCADZyrXxgBTahQ4sPyRRj7GrhMEu_DkmScRRGOjRJQmd2rkH1-_K0WRjfGYd04rhJB/pub">Creators' Guide</Anchor>
+	  for how to make your own dreams, and the
+	  {" "}<Anchor href="https://github.com/e-dream-ai/python-api">Python API</Anchor>
+	  for how to connect your own code.  Or watch the following installation video walkthrough.
 	</Text>
-	<h3>Installation Video Walkthrough</h3>
         <Column flex="auto" style={{ height: "70vh", minHeight: "800px" }}>
           <JekyllSiteViewer />
         </Column>
