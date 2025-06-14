2a4b9ab9fd41d4f27c5be8aea3b3714b08367fd2
Scott Draves <spot@draves.org>
2025-06-14 09:09:09 -0400

    combine text blocks for better layout

--- src/components/pages/create/create-dream.tsx
+++ src/components/pages/create/create-dream.tsx
@@ -112,8 +112,8 @@ export const CreateDream: React.FC = () => {
           </>
         ) : (
           <>
-            <Text my={3}>{t("page.create.dream_instructions")}</Text>
-	    <Text my={3}>
+            <Text my={3}>
+	      {t("page.create.dream_instructions")}
 	      See the {" "}<Anchor href="https://docs.google.com/document/u/1/d/e/2PACX-1vTQnJMCLOqenrCADZyrXxgBTahQ4sPyRRj7GrhMEu_DkmScRRGOjRJQmd2rkH1-_K0WRjfGYd04rhJB/pub">Creators' Guide</Anchor> and
 	      {" "}<Anchor href="https://github.com/e-dream-ai/python-api">Python API</Anchor> for more ways to make and share dreams.
 	    </Text>
