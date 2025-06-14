5657c83bd84d2eb9744fa9ad32dfbc862de1a941
Scott Draves <spot@draves.org>
2025-06-14 12:23:56 -0400

    add more help links

--- src/components/pages/help/help.page.tsx
+++ src/components/pages/help/help.page.tsx
@@ -1,7 +1,8 @@
-import { Column, Row } from "@/components/shared";
+import { Column, Row, Anchor } from "@/components/shared";
 import Container from "@/components/shared/container/container";
 import JekyllSiteViewer from "@/components/shared/jekyll-viewer/jekyll-viewer";
 import { Section } from "@/components/shared/section/section";
+import Text from "@/components/shared/text/text";
 import { useTranslation } from "react-i18next";
 
 const SECTION_ID = "help";
@@ -13,6 +14,13 @@ export const HelpPage: React.FC = () => {
       <h2>{t("page.help.title")}</h2>
       <Section id={SECTION_ID}>
         <Row justifyContent="space-between" separator />
+	<h3>Links to Documentation</h3>
+	<Text>
+	  See the Users Manual for explanation of the app and website,
+	  {" "}<Anchor href="https://docs.google.com/document/u/1/d/e/2PACX-1vTQnJMCLOqenrCADZyrXxgBTahQ4sPyRRj7GrhMEu_DkmScRRGOjRJQmd2rkH1-_K0WRjfGYd04rhJB/pub">Creators' Guide</Anchor>, and the
+	  {" "}<Anchor href="https://github.com/e-dream-ai/python-api">Python API</Anchor>.
+	</Text>
+	<h3>Installation Video Walkthrough</h3>
         <Column flex="auto" style={{ height: "70vh", minHeight: "800px" }}>
           <JekyllSiteViewer />
         </Column>
