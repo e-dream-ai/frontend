785797c1cd3d00a7ef6a17a7543e15f0cf26e2a1
Scott Draves <spot@draves.org>
2025-06-14 12:43:51 -0400

    formatting

--- src/components/pages/help/help.page.tsx
+++ src/components/pages/help/help.page.tsx
@@ -16,10 +16,10 @@ export const HelpPage: React.FC = () => {
         <Row justifyContent="space-between" separator />
 	<Text>
           See the {" "}<Anchor href="https://docs.google.com/document/d/e/2PACX-1vRK77s1OWASmJ_r8ZZXkFzNxpSxoqcOVn2-0shwXZquzxsWfzO86oeH_9Q09IVmb5gaUnuHZQP0ZihG/pub">Users' Manual</Anchor> for explanation of the app and website, the
-	  {" "}<Anchor href="https://docs.google.com/document/u/1/d/e/2PACX-1vTQnJMCLOqenrCADZyrXxgBTahQ4sPyRRj7GrhMEu_DkmScRRGOjRJQmd2rkH1-_K0WRjfGYd04rhJB/pub">Creators' Guide</Anchor>
+	  {" "}<Anchor href="https://docs.google.com/document/u/1/d/e/2PACX-1vTQnJMCLOqenrCADZyrXxgBTahQ4sPyRRj7GrhMEu_DkmScRRGOjRJQmd2rkH1-_K0WRjfGYd04rhJB/pub">Creators' Guide</Anchor>{" "}
 	  for how to make your own dreams, and the
-	  {" "}<Anchor href="https://github.com/e-dream-ai/python-api">Python API</Anchor>
-	  for how to connect your own code.  Or watch the following installation video walkthrough.
+	  {" "}<Anchor href="https://github.com/e-dream-ai/python-api">Python API</Anchor>{" "}
+	  for how to connect your own code.  Or watch the following installation and basic usage walkthrough:
 	</Text>
         <Column flex="auto" style={{ height: "70vh", minHeight: "800px" }}>
           <JekyllSiteViewer />
