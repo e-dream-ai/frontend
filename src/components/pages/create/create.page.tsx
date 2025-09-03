32e5a7ff21fcd18006a3681452c5851ffc1bae93
Scott Draves <spot@draves.org>
2025-09-03 00:10:26 -0400

    move initial tab if you have no perms to the 1st

--- src/components/pages/create/create.page.tsx
+++ src/components/pages/create/create.page.tsx
@@ -31,7 +31,7 @@ export const CreatePage: React.FC = () => {
   const { t } = useTranslation();
   const navigate = useNavigate();
   const location = useLocation();
-  const [tabIndex, setTabIndex] = useState(0);
+  const [tabIndex, setTabIndex] = useState(DREAM_PERMISSIONS.CAN_VIEW_BECOME_CREATOR ? 1 : 0);
 
   useEffect(() => {
     switch (location.pathname) {
