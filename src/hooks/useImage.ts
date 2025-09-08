d2ce0f8339075772908eda1d0f7c971f1caf644c
mystixxx <okejhere@gmail.com>
2025-09-08 17:58:34 +0200

    fix for build

--- src/hooks/useImage.ts
+++ src/hooks/useImage.ts
@@ -1,6 +1,8 @@
+import { ResizeOptions } from "@/types/image.types";
 import { useMemo } from "react";
 
-export const useImage = (url?: string) => {
+export const useImage = (url?: string, resizeOptions?: ResizeOptions) => {
+  console.log("resizeOptions", resizeOptions);
   return useMemo(() => {
     return url;
   }, [url]);
